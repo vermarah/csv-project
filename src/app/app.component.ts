46831c20b928c1d5ec763153fe050a15a61a9baa
Rahul Verma <vermarah@deakin.edu.au>
2022-03-23 11:45:48 +1100

    updating the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -64,9 +64,8 @@ export class AppComponent implements OnInit{
   fileChangeListener($event: any): void {
 
     const files = $event.srcElement.files;
-    this.header = (this.header as unknown as string) === 'true' || this.header === true;
 
-    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ',' })
+    this.ngxCsvParser.parse(files[0], {delimiter: ',' })
       .pipe().subscribe((result: Array<any>) => {
         console.log('Result', result);
         this.csvRecords = result;
@@ -78,9 +77,8 @@ export class AppComponent implements OnInit{
   fileChangeListener1($event: any): void {
 
     const files1 = $event.srcElement.files;
-    this.header = (this.header as unknown as string) === 'true' || this.header === true;
 
-    this.ngxCsvParser.parse(files1[0], { header: this.header, delimiter: ',' })
+    this.ngxCsvParser.parse(files1[0], {delimiter: ',' })
       .pipe().subscribe((result1: Array<any>) => {
         console.log('Result1', result1);
         this.csvRecords1 = result1;
